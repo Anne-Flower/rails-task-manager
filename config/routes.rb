e33e73f86b106104a069abd99897ad1d9815ba3c
AnneFlore Bernard <b.anneflore@yahoo.fr>
2023-08-15 16:12:33 +0200

    amazing design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  # # root to: "tasks#index"
+  # get '/tasks', to:'tasks#index' 
+  # get "tasks/:id/edit", to: "tasks#edit", as:'edit'
+  # get 'tasks/new', to: 'tasks#new', as:'new'
+  # post 'task/create', to: 'tasks#create'
+  # get 'tasks/:id', to:'tasks#show', as:'task'
+  # delete "tasks/:id", to: "tasks#destroy", as: 'destroy'
+  resources :tasks
 end
